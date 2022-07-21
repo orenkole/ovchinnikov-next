b15c5d30d9d990c3b3ae8c8446f1a0ebcdd242a7
orenkole <orenkole@gmail.com>
2022-07-21 09:30:39 +0300

    Custom hook useId

--- coursesbox/components/Checkbox/Checkbox.tsx
+++ coursesbox/components/Checkbox/Checkbox.tsx
@@ -1,6 +1,7 @@
 import {FC, ChangeEvent, useRef} from "react";
 import styled from "@emotion/styled";
 import {boxShadow, transition} from "@/components/style";
+import {useId} from "@/components/hooks/useId";
 
 const Wrapper = styled.label`
   font-size: 1.5rem;
@@ -38,9 +39,7 @@ type Props = {
 }
 
 export const Checkbox: FC<Props> = ({onChange}) => {
-    const {current: fieldId} = useRef(
-        `prefix-${Math.random().toString(16).slice(2)}`
-    )
+    const fieldId = useId()
     return (
         <Wrapper>
             <input id={fieldId} type="checkbox" onChange={onChange} />
